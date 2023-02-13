f86328e1153e8ec59b1a25247af832608ffeb06d
주은오 <jueun-o@jueun-oui-MacBookPro.local>
2023-02-13 02:05:58 -0800

    first commit

---
+++ src/main/java/com/example/demo/SocietyApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyApplication.class, args);
+		//yuk test2
+	}
+
+}
